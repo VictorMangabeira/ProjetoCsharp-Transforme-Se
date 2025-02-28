21f564b70984350d5deb181860be4355694afc89
victormangabeira <victor.omangabeira@senacsp.edu.br>
2025-02-28 17:21:25 -0300

    janela de menu principal

--- LojaABC/frmLogin.cs
+++ LojaABC/frmLogin.cs
@@ -16,5 +16,15 @@ namespace LojaABC
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSair_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
     }
 }
