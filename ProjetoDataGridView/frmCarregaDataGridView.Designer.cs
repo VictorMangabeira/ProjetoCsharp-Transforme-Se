32b025eb4ca52e34f26dca408c4d6a35ce27f872
victormangabeira <victor.omangabeira@senacsp.edu.br>
2025-04-24 14:38:03 -0300

    conectando data grid view

---
+++ ProjetoDataGridView/frmCarregaDataGridView.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace ProjetoDataGridView
+{
+    partial class frmCarregaDataGridView
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvDados = new System.Windows.Forms.DataGridView();
+            this.btnCarregaGridView = new System.Windows.Forms.Button();
+            this.btnLimpar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDados)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvDados
+            // 
+            this.dgvDados.AllowUserToAddRows = false;
+            this.dgvDados.AllowUserToDeleteRows = false;
+            this.dgvDados.AllowUserToResizeColumns = false;
+            this.dgvDados.AllowUserToResizeRows = false;
+            this.dgvDados.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvDados.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.DisableResizing;
+            this.dgvDados.Location = new System.Drawing.Point(22, 12);
+            this.dgvDados.Name = "dgvDados";
+            this.dgvDados.Size = new System.Drawing.Size(755, 321);
+            this.dgvDados.TabIndex = 0;
+            // 
+            // btnCarregaGridView
+            // 
+            this.btnCarregaGridView.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCarregaGridView.Location = new System.Drawing.Point(21, 354);
+            this.btnCarregaGridView.Name = "btnCarregaGridView";
+            this.btnCarregaGridView.Size = new System.Drawing.Size(147, 45);
+            this.btnCarregaGridView.TabIndex = 1;
+            this.btnCarregaGridView.Text = "Carrega Grid View";
+            this.btnCarregaGridView.UseVisualStyleBackColor = true;
+            this.btnCarregaGridView.Click += new System.EventHandler(this.btnCarregaGridView_Click);
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLimpar.Location = new System.Drawing.Point(203, 354);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(121, 45);
+            this.btnLimpar.TabIndex = 2;
+            this.btnLimpar.Text = "Limpar";
+            this.btnLimpar.UseVisualStyleBackColor = true;
+            this.btnLimpar.Click += new System.EventHandler(this.btnLimpar_Click);
+            // 
+            // frmCarregaDataGridView
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnLimpar);
+            this.Controls.Add(this.btnCarregaGridView);
+            this.Controls.Add(this.dgvDados);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.Name = "frmCarregaDataGridView";
+            this.Text = "frmCarregaDataGridView";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDados)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvDados;
+        private System.Windows.Forms.Button btnCarregaGridView;
+        private System.Windows.Forms.Button btnLimpar;
+    }
+}
---
+++ ProjetoDataGridView/frmCarregaDataGridView.cs
@@ -0,0 +1,50 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace ProjetoDataGridView
+{
+    public partial class frmCarregaDataGridView : Form
+    {
+        public frmCarregaDataGridView()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCarregaGridView_Click(object sender, EventArgs e)
+        {
+            MySqlCommand comm = new MySqlCommand();
+            comm.CommandText = " select codFunc as 'Codigo', nome as 'Nome', email as 'E-mail', lougradouro as 'Endereço', cep as 'Número' from tbFuncionarios;";
+            comm.CommandType = CommandType.Text;
+
+            comm.Connection = Conexao.obterConexao();
+
+            MySqlDataAdapter adapter = new MySqlDataAdapter(comm);
+
+            DataTable dataTable = new DataTable();
+
+            adapter.Fill(dataTable);
+
+            dgvDados.DataSource = dataTable;
+
+            Conexao.fecharConexao();
+
+        }
+
+        private void btnLimpar_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < dgvDados.RowCount; i++)
+            {
+                dgvDados.Rows[i].DataGridView.Columns.Clear();
+            }
+        }
+    }
+}
